77ef281116e6c026a625a8a9a0b5cfc87389aeb2
BurntIce <BurntIce@users.noreply.github.com>
2018-10-07 20:06:20 +0800

    Do a number of testings on arrays of ascending and descending orders
    Previously, numerous testings was done only for randomly-ordered arrays. Turns out CPU time fluctuate too much for the other two types of arrays.
    Cut down on max array size and number of testings to reduce runtime

--- src/InsertionVersusMerge/InsertionVersusMerge.java
+++ src/InsertionVersusMerge/InsertionVersusMerge.java
@@ -23,10 +23,10 @@ public class InsertionVersusMerge
 	 * can be changed to console input later on
 	 */
 	private static final int MIN_ARRAY_SIZE = 1000;
-	private static final int MAX_ARRAY_SIZE = 100_000;
+	private static final int MAX_ARRAY_SIZE = 50_000;
 	private static final int STEP_SIZE = 1000;
 	
-	private static final int NUM_OF_TESTINGS_RANDOM = 100;
+	private static final int NUM_OF_TESTINGS = 60;
 	
 	/**
 	 * We need to compare between insertion sort and merge sort for arrays of different types and sizes.
@@ -73,54 +73,74 @@ public class InsertionVersusMerge
 			 */
 			
 			/**
-			 * We do not want the result of sorting through a randomly-ordered array to be skewed by extreme cases
-			 * e.g. an array randomly generated turns out to be in descending order
-			 * Therefore, for a particular array size, we sort a number of randomly-generated arrays of that size
+			 * We do not want the result of sorting skewed by extreme cases
+			 * e.g. fluctuating CPU time, or an array randomly generated turns out to be in descending order
+			 * Therefore, for a particular array size, we sort a number of arrays of that size
 			 * and take the average as the final result.
 			 */
-			StatisticalResults[] insertRandomResultsArray = new StatisticalResults[NUM_OF_TESTINGS_RANDOM];
-			StatisticalResults[] mergeRandomResultsArray = new StatisticalResults[NUM_OF_TESTINGS_RANDOM];
+			StatisticalResults[] insertRandomResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
+			StatisticalResults[] mergeRandomResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
 			
-			// generate, sort and store all testings' results
-			for(int testingsIndex = 0; testingsIndex < NUM_OF_TESTINGS_RANDOM; ++testingsIndex)
+			StatisticalResults[] insertAscendResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
+			StatisticalResults[] mergeAscendResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
+			
+			StatisticalResults[] insertDescendResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
+			StatisticalResults[] mergeDescendResultsArray = new StatisticalResults[NUM_OF_TESTINGS];
+			
+			for(int testingsIndex = 0; testingsIndex < NUM_OF_TESTINGS; ++testingsIndex)
 			{
 				insertRandomResultsArray[testingsIndex] = new StatisticalResults(arraySize);
 				mergeRandomResultsArray[testingsIndex] = new StatisticalResults(arraySize);
 				
+				insertAscendResultsArray[testingsIndex] = new StatisticalResults(arraySize);
+				mergeAscendResultsArray[testingsIndex] = new StatisticalResults(arraySize);
+				
+				insertDescendResultsArray[testingsIndex] = new StatisticalResults(arraySize);
+				mergeDescendResultsArray[testingsIndex] = new StatisticalResults(arraySize);
+				
+				// generate random, ascending and descending arrays for sorting
 				long[] insertRandomArray = InputGenerator.generateRandomArray2(arraySize);
 				long[] mergeRandomArray = InputGenerator.generateRandomArray2(arraySize);
 				
+				long[] insertAscendArray = InputGenerator.generateAscendArray(arraySize);
+				long[] mergeAscendArray = InputGenerator.generateAscendArray(arraySize);
+				
+				long[] insertDescendArray = InputGenerator.generateDescendArray(arraySize);
+				long[] mergeDescendArray = InputGenerator.generateDescendArray(arraySize);
+				
+				// sort random, ascending and descending arrays using insertion and merge sort
 				Sorter.insertionSort(insertRandomArray, insertRandomResultsArray[testingsIndex]);
 				Sorter.mergeSort(mergeRandomArray, 0, arraySize-1, mergeRandomResultsArray[testingsIndex]);
 				
-				if(testingsIndex % (NUM_OF_TESTINGS_RANDOM/10) == 0)
-					System.out.println("Random Sort Progress: " + 
-							(testingsIndex/(NUM_OF_TESTINGS_RANDOM/10)+1) + " of 10");
+				Sorter.insertionSort(insertAscendArray, insertAscendResultsArray[testingsIndex]);
+				Sorter.mergeSort(mergeAscendArray, 0, arraySize-1, mergeAscendResultsArray[testingsIndex]);
+				
+				Sorter.insertionSort(insertDescendArray, insertDescendResultsArray[testingsIndex]);
+				Sorter.mergeSort(mergeDescendArray, 0, arraySize-1, mergeDescendResultsArray[testingsIndex]);
+				
+				if(testingsIndex % (NUM_OF_TESTINGS/10) == 0)
+					System.out.println("Testing Progress: " + 
+							(testingsIndex/(NUM_OF_TESTINGS/10)+1) + " of 10");
 			}
 			
-			/**
-			 * no need to do a number of testings for ascending and descending array
-			 */
-			// average and store final result of sorting randomly-ordered array using insertion and merge sort
-			Sorter.quickAndInsertionSort(insertRandomResultsArray,0,NUM_OF_TESTINGS_RANDOM-1);
+			// average and store final result of sorting arrays using insertion and merge sort
+			Sorter.quickAndInsertionSort(insertRandomResultsArray,0,NUM_OF_TESTINGS-1);
 			sortingResultsArray[0].setAverageCPUTimeAndNumOfKeyCmp(insertRandomResultsArray);
 			
-			Sorter.quickAndInsertionSort(mergeRandomResultsArray,0,NUM_OF_TESTINGS_RANDOM-1);
+			Sorter.quickAndInsertionSort(insertAscendResultsArray,0,NUM_OF_TESTINGS-1);
+			sortingResultsArray[1].setAverageCPUTimeAndNumOfKeyCmp(insertAscendResultsArray);
+			
+			Sorter.quickAndInsertionSort(insertDescendResultsArray,0,NUM_OF_TESTINGS-1);
+			sortingResultsArray[2].setAverageCPUTimeAndNumOfKeyCmp(insertDescendResultsArray);
+			
+			Sorter.quickAndInsertionSort(mergeRandomResultsArray,0,NUM_OF_TESTINGS-1);
 			sortingResultsArray[3].setAverageCPUTimeAndNumOfKeyCmp(mergeRandomResultsArray);
 			
-			// generate ascending and descending arrays with insertion and merge sort
-			long[] insertAscendArray = InputGenerator.generateAscendArray(arraySize);
-			long[] insertDescendArray = InputGenerator.generateDescendArray(arraySize);
+			Sorter.quickAndInsertionSort(mergeAscendResultsArray,0,NUM_OF_TESTINGS-1);
+			sortingResultsArray[4].setAverageCPUTimeAndNumOfKeyCmp(mergeAscendResultsArray);
 			
-			long[] mergeAscendArray = InputGenerator.generateAscendArray(arraySize);
-			long[] mergeDescendArray = InputGenerator.generateDescendArray(arraySize);
-
-			// sort ascending and descending arrays with insertion and merge sort
-			Sorter.insertionSort(insertAscendArray, sortingResultsArray[1]);
-			Sorter.insertionSort(insertDescendArray, sortingResultsArray[2]);
-			
-			Sorter.mergeSort(mergeAscendArray, 0, arraySize-1, sortingResultsArray[4]);
-			Sorter.mergeSort(mergeDescendArray, 0, arraySize-1, sortingResultsArray[5]);
+			Sorter.quickAndInsertionSort(mergeDescendResultsArray,0,NUM_OF_TESTINGS-1);
+			sortingResultsArray[5].setAverageCPUTimeAndNumOfKeyCmp(mergeDescendResultsArray);
 			
 			// transfer of sorting results to Excel workbook
 			for(int index = 0; index < sortingResultsArray.length; ++ index)
@@ -128,9 +148,9 @@ public class InsertionVersusMerge
 				IOHandler.transferResultsToWorkbook(sortingResultsArray[index], resultsWorkbookArray[index]);
 			}
 			
-			if(arraySize % (MAX_ARRAY_SIZE/100) == 0)
-				System.out.println("Overall Progress: " + arraySize/(MAX_ARRAY_SIZE/100) + 
-									" of 100\n--------------------------");
+			if(arraySize % STEP_SIZE == 0)
+				System.out.println("Overall Progress: " + arraySize/STEP_SIZE + 
+									" of " + MAX_ARRAY_SIZE/STEP_SIZE + "\n--------------------------");
 		}
 		
 		// create appropriate file names, write results to files and close workbooks
